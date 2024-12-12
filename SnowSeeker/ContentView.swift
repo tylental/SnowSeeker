d04a4a512c5bbe29983d5012ca8dd01e2a75fb64
tylental <101352437+tylental@users.noreply.github.com>
2024-12-11 22:27:08 -0500

    Working with split views side by side

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hi update change")
+            // SwiftUI will differ automatically show different views on different devices
+            NavigationSplitView(preferredCompactColumn: .constant(.detail)){
+                // tapping on the link will display over the secondary/detail view
+                NavigationLink("Primary"){
+                    Text("New View")
+                }
+            } detail: {
+                Text("Content")
+            }
+            // show our views within the split view as equal portions of each other, if the view is permissible
+            .navigationSplitViewStyle(.balanced)
         }
         .padding()
     }
