3bcb97cac403da928774c8f193d5a47ecfd75619
tylental <101352437+tylental@users.noreply.github.com>
2024-12-18 22:46:59 -0500

    Implement SwiftUI search bar

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,39 +7,33 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        // using Group allows our parent view to dictate how views will be laid out for views within the Group
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Donald and Trump")
-        }
-        .font(.title)
-    }
-}
 
 struct ContentView: View {
-    // watches how our view should be handled depending on the orientation boundaries of our device
+    @State private var searchText = ""
     
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    let allNames = ["Tyler", "Peter", "Iron Man", "Joey"]
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        }
+        else {
+            allNames.filter { name in
+                
+                // best way to find a substring of another string and ignores case by default
+                name.localizedStandardContains(searchText)
+            }
+        }
+    }
+    
     
     var body: some View {
-//        if horizontalSizeClass == .compact {
-//            // syntactically different way to call our view
-//                VStack(content: UserView.init)
-//            } else {
-//                HStack(content: UserView.init)
-//            }
-        
-        // showing the best view that is suitable and fits the requirements of our device
-        
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-            
-            Circle()
-                .frame(width: 200, height: 200)
+        NavigationStack {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+
         }
         
     }
