dad13c025725d0bc18cd79386a2253af2e462008
tylental <101352437+tylental@users.noreply.github.com>
2024-12-19 22:34:11 -0500

    Add layout modifiers to adapt for different sizes

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ResortView: View {
     let resort: Resort
     
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
@@ -18,11 +21,25 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    
+                    // checks for the best layout to use when the conditions of these sizes meet
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing: 10){
+                            ResortDetailsView(resort: resort)
+                        }
+                        VStack(spacing: 10) {
+                            SkiDetailsView(resort: resort)
+                        }
+                        
+                        // otherwise, just line them up in the HStack if there is space
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .padding(.vertical)
                 .background(.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
                 
                 Group {
                     Text(resort.description)
